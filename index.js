d1810f21706cb7de223339ced88f199b1d249d47
Numinor <numinor93@mail.ru>
2019-07-12 16:46:50 +0300

    Finished rock-paper-scissors

--- index.js
+++ index.js
@@ -12,21 +12,36 @@ function playRound(playerSelection, computerSelection) {
     if((playerSelection === 'rock' && computerSelection === 'scissors') || 
        (playerSelection === 'paper' && computerSelection === 'rock') ||
        (playerSelection === 'scissors' && computerSelection === 'paper')) {
-            return `You won ${playerSelection} beats ${computerSelection}`;
-       } else {
-           return `Computer won ${computerSelection} beats ${playerSelection}`;
+           data.score += 1;
+            return `You won! ${playerSelection} beats ${computerSelection}.`;
+       } else 
+
+       if(playerSelection === computerSelection) {
+            return "It's a draw.";
+       } else 
+       
+       {
+            return `Computer won! ${computerSelection} beats ${playerSelection}.`;
        }
 }
 
-function game() {
-    setup();
-    while(data.gameLenght > 0) {
-        playRound()
-    }
+function setup() {
+    let playerSelection;
+    while(playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
+        playerSelection = prompt("Input rock, paper or scissors").toLowerCase();
+    };
+    let computerSelection = computerPlay();
+
+    return [playerSelection, computerSelection];
 }
 
-function setup() {
-    const playerSelection = prompt("Input your decision").toLowerCase();
-    const computerSelection = computerPlay();
+function game() {
+    do {
+        data.gameLenght = prompt("Input desired amount of rounds. (positive integer)");
+    } while (Number.isInteger(data.gameLenght));
+    while(data.gameLenght > 0) {
+        let [x,y] = setup();
+        console.log(playRound(x,y), `Your score is ${data.score}`);
+        data.gameLenght -= 1;
+    }
 }
-console.log(playRound(playerSelection,computerSelection))
